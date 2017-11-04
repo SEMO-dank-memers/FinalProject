e5b4372cc867befbe5991df99faaf724dd9fd749
rocke97 <aarongunther97@gmail.com>
2017-11-03 19:28:19 -0500

    Added the start of the upgrade system
    The game now has a basic upgrade ui that dosent work yet and a play
    button that restarts the game after a players run, saving their current
    level in each upgrade field in order to recalculate the amount of pushes
    the player has unlocked so far.

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartGame : MonoBehaviour {
+	public GameObject playerToSave;
+	public Button playButton;
+	public static int currentForwardPushLevel;
+	public static int currentUpwardPushLevel;
+
+	private static GameObject objectInstance;
+	void Awake(){
+		DontDestroyOnLoad (this);
+		if (objectInstance == null) {
+			objectInstance = gameObject;
+		} 
+		else {
+			DestroyObject (gameObject);
+			//have to reset up the play button here in order for the restart to work twice.
+			playerToSave.GetComponent<ZippyTerrain2DRollingBall> ().upwardPushLevel = currentUpwardPushLevel;
+			playerToSave.GetComponent<ZippyTerrain2DRollingBall> ().forwardPushLevel = currentForwardPushLevel;
+		}
+	}
+
+	public void Restart(){
+		//oh god there is so many better ways to do this but the time isn't worth it.....
+		//where is your god now encapsulation?? WHERE?
+		currentForwardPushLevel = playerToSave.GetComponent<ZippyTerrain2DRollingBall> ().forwardPushLevel;
+		currentUpwardPushLevel = playerToSave.GetComponent<ZippyTerrain2DRollingBall> ().upwardPushLevel;
+		SceneManager.LoadScene ("Main");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
--- Assets/Zippy 2D/Zippy Terrain 2D/Demo/Scripts/ZippyTerrain2DRollingBall.cs
+++ Assets/Zippy 2D/Zippy Terrain 2D/Demo/Scripts/ZippyTerrain2DRollingBall.cs
@@ -1,33 +1,45 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 public class ZippyTerrain2DRollingBall : MonoBehaviour {
 
 	//public vars
+
 	[Header("Static/Starting Traits")]
+	[Tooltip("Canvas that contains the Upgrade GUI in order to display when the game ends")]
+	public Canvas gameCanvas;
 	[Range(2.0f, 10.0f)] // bounds the inital force for testing and tweaking between these values
 	[Tooltip("Initial force applied to the rock when the game starts")]
-	public float initialForce=5.0f; // the initial force applied to the rock when the game starts
+	public float initialForce; // the initial force applied to the rock when the game starts
 	[Tooltip("Initial force applied by all of the player's pushes, the upgrade level multiples this value")]
 	[Range(5.0f, 30.0f)]
-	public float initialPushForce=5.0f; // the force applied when the player uses a wind push, can be increased with the upgrades
+	public float initialPushForce; // the force applied when the player uses a wind push, can be increased with the upgrades
+	[Tooltip("Initial amount of forward pushes, this is multiplied in order to determine the number of pushes gained per level")]
+	public int initialForwardPushes;
+	[Tooltip("Initial amount of upward pushes, this is multiplied in order to determine the number of pushes gained per level")]
+	public int initialUpwardPushes;
 	[Tooltip("The current money of the player")]
 	public int playerMoney; //game money, making public so that other events can increase it
+
 	[Header("Upgradeable Traits")]
 	[Tooltip("The number of forward pushes the player can use")]
-	public int forwardPushes = 3; //number of times the player can push the rock forward
+	public int forwardPushes; //number of times the player can push the rock forward
 	[Tooltip("The current upgrade level of the player's forward pushs. This is the amount the initialForwardForce is multiplied by.")]
-	public int forwardPushLevel = 1; //current upgrade level of the player's forward Pushes;
+	public int forwardPushLevel; //current upgrade level of the player's forward Pushes;
 	[Tooltip("The number of upward pushes the player can use")]
-	public int upwardPushes = 3; //number of times the player can push the rock upward
+	public int upwardPushes; //number of times the player can push the rock upward
 	[Tooltip("The current upgrade level of the player's upward pushs. This is the amount the initialForwardForce is multiplied by.")]
-	public int upwardPushLevel = 1; //current upgrade level of the player's upward pushes;
+	public int upwardPushLevel; //current upgrade level of the player's upward pushes;
 	//
 
 	Rigidbody2D cacheRB;
 	float input;
+	bool gameOver = false;
 	void Start () {
 		cacheRB = GetComponent<Rigidbody2D>();
 		cacheRB.AddForce(new Vector2(initialForce, 0.0f), ForceMode2D.Impulse);
+		forwardPushes = initialForwardPushes * forwardPushLevel;
+		upwardPushes = initialUpwardPushes * upwardPushLevel;
 	}
 
 	void OnTriggerEnter2D(Collider2D coll){
@@ -35,6 +47,13 @@ public class ZippyTerrain2DRollingBall : MonoBehaviour {
 			coll.gameObject.SetActive (false);
 		}
 		playerMoney = playerMoney + 5;
+	}
+
+	IEnumerator CountDown (){ //this triggers the end of the game
+		yield return new WaitForSecondsRealtime (5);
+		gameOver = true;
+		gameCanvas.transform.GetChild (1).GetComponent<Text>().text = "  Player Money: " + playerMoney; //god this is bad
+		gameCanvas.gameObject.SetActive (true);
 	}
 
 	void FixedUpdate() {
@@ -48,6 +67,11 @@ public class ZippyTerrain2DRollingBall : MonoBehaviour {
 			if (upwardPushes != 0) {
 				cacheRB.AddForce (new Vector2 (0.0f, initialPushForce*upwardPushLevel), ForceMode2D.Impulse);
 				upwardPushes--;
+			}
+		}
+		if ((upwardPushes == 0) && (forwardPushes == 0) && (!gameOver)) { //if we are out of ways to speed up the rock and its going backwards then the game is over
+			if((cacheRB.velocity.x == 0) || (cacheRB.velocity.x < Vector2.zero.x)){
+				StartCoroutine ("CountDown");
 			}
 		}
 	}
