8c95b6ac52f637768511957fb40691e8d8d37306
Stephen <scsladek91@gmail.com>
2017-11-06 10:56:00 -0600

    Started an enemy generator

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StateMachine : MonoBehaviour{
-	/* States:
+    /* States:
 	 * Idle is just standing and doing nothing; used for initializing
 	 * Throw takes place when a Thrower is brave; a rock is thrown
 	 * Attack is used when brawler is brave, or when a troll has max brave value; throws a punch
@@ -14,7 +14,7 @@ public class StateMachine : MonoBehaviour{
 	 * Fly is the initial state of the bird; Flies forward at a steady rate
 	 * Charge is used by a flock of aggroed birds; Charges toward the boulder in a kamikazi attack
 	 */
-	/* Roles:
+    /* Roles:
 	 * Unassigned is a null initializer
 	 * Brawler is the bald goblin that is more likely to attack
 	 * Thrower is the samurai looking goblin that is more likely to throw or run
@@ -22,47 +22,62 @@ public class StateMachine : MonoBehaviour{
 	 * Ninja is the goblin in black that will run, jump, or crouch
 	 * Bird is bird, it flies and a whole flock gets mad if you hit their friend
 	 */
-	public class Enemy{
-		enum State { IDLE, THROW, ATTACK, RUN, CROUCH, JUMP, PANIC, FLY, CHARGE };
-		enum Role { UNASSIGNED, BRAWLER, THROWER, TROLL, NINJA, BIRD };
-		State currentState {get; set;}
-		Role role {get; set;}
-		float isBrave, isAfraid; //used with fuzzy logic to help determine the State, for variation purposes
-		//pass by value constructor
-		Enemy(State state, Role role, float b){ 
-			this.currentState = state;
-			this.role = role;
-			this.isBrave = b;
-			this.isAfraid = Fuzzy.NOT(isBrave);
-		}
-		void setBravery (Role role)
-		{
-			//threshold values are set according the Role
-			float randomNum = Random.Range(0.0f, 10.0f); //using values of 0 - 10 to keep simple-ish
-			float lowerThreshold = 4.0f, upperThreshold = 7.0f;
-			switch(role){
-			case Role.BRAWLER:
-				lowerThreshold = 2.0f;
-				upperThreshold = 5.0f;
-				break;
-			case Role.THROWER:
-				lowerThreshold = 3.5f;
-				upperThreshold = 7.0f;
-				break;
-			case Role.TROLL:
-				lowerThreshold = 7.0f;
-				upperThreshold = 9.5f;
-				break;
-			case Role.NINJA:
-				lowerThreshold = 2.0f;
-				upperThreshold = 7.0f;
-				break;
-			default:
-				break; //can just break since we have initialized values
-			}
-			//call upon chosen fuzzy function using the above floats
-			this.isBrave = Fuzzy.Linear(randomNum,lowerThreshold,upperThreshold);
-			this.isAfraid = Fuzzy.NOT(isBrave); //same as NOT(isBrave)
-		}
-	}
+    public class Enemy
+    {
+        public enum State { IDLE, THROW, ATTACK, RUN, CROUCH, JUMP, PANIC, FLY, CHARGE };
+        public enum Role { UNASSIGNED, BRAWLER, THROWER, TROLL, NINJA, BIRD };
+        public State currentState { get; set; }
+        public Role role { get; set; }
+        public float isBrave, isAfraid; //used with fuzzy logic to help determine the State, for variation purposes
+        public Enemy() { }
+        //pass by value constructor
+        public Enemy(State state, Role role, float b)
+        {
+            this.currentState = state;
+            this.role = role;
+            this.isBrave = b;
+            this.isAfraid = Fuzzy.NOT(isBrave);
+        }
+        public void setBravery(Role role)
+        {
+            //threshold values are set according the Role
+            float randomNum = Random.Range(0.0f, 10.0f); //using values of 0 - 10 to keep simple-ish
+            float lowerThreshold = 4.0f, upperThreshold = 7.0f;
+            switch (role)
+            {
+                case Role.BRAWLER:
+                    lowerThreshold = 2.0f;
+                    upperThreshold = 5.0f;
+                    break;
+                case Role.THROWER:
+                    lowerThreshold = 3.5f;
+                    upperThreshold = 7.0f;
+                    break;
+                case Role.TROLL:
+                    lowerThreshold = 7.0f;
+                    upperThreshold = 9.5f;
+                    break;
+                case Role.NINJA:
+                    lowerThreshold = 2.0f;
+                    upperThreshold = 7.0f;
+                    break;
+                default:
+                    break; //can just break since we have initialized values
+            }
+            //call upon chosen fuzzy function using the above floats
+            this.isBrave = Fuzzy.Linear(randomNum, lowerThreshold, upperThreshold);
+            this.isAfraid = Fuzzy.NOT(isBrave); //same as NOT(isBrave)
+        }
+        Enemy createEnemy()
+        {
+            Enemy enemy = new Enemy();
+            float rand = Random.Range(0.0f, 10.0f);
+            if (rand < 5.0f && rand >= 2.5f) enemy.role = Role.THROWER;
+            else if (rand < 7.5f && rand >= 5.0f) enemy.role = Role.BRAWLER;
+            else if (rand < 10.0f && rand >= 7.5f) enemy.role = Role.NINJA;
+            else enemy.role = Role.TROLL;
+            enemy.setBravery(enemy.role);
+            return enemy;
+        }
+    }
 }
