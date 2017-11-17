b2b4a6aa7ebe3b7fc6e4ff3ae8a6e8b4e94d60ce
Sladek <scsladek1s@semo.edu>
2017-11-17 10:55:13 -0600

    Made small gains on the enemy behaviour

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+        // m_someOtherScriptOnAnotherGameObject = GameObject.FindObjectOfType(typeof(ScriptA)) as ScriptA;
+        // m_someOtherScriptOnAnotherGameObject.Test();
+        // or  myObject.GetComponent<MyScript>().MyFunction();
+        StateMachine.Enemy foolToBeCrushed = enemy.GetComponent<StateMachine.Enemy>().GenerateEnemy();
+        StateMachine.Enemy.Role role = enemy.GetComponent<StateMachine.Enemy>().role;
+        enemy.GetComponent<StateMachine.Enemy>().GenerateSprite(role);
+        foolToBeCrushed.SetBravery(role);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    //run change state logic
+        //set behaviour according to the set state
+	}
+
+    void Idle()
+    {
+        /* just stands still and detects if a rock is nearby
+         * if the rock is very far away, as in this enemy is now far off screen...
+         * we should call a destructor to get rid of this ai so we're not taking up excess resources
+         * Destroy(this);
+         */
+    }
+    void Attack()
+    {
+        //gets the position of the rock and moves towards it. 
+        //when at a close distance, throw a punch.
+    }
+    void Punch()
+    {
+        //uses a punching animation, and reduces momentum more significantly than a throw if it hits.
+
+    }
+    void Run()
+    {
+        /* gets position of the rock and moves away from it, might need to be careful to not
+         * let them move into the air. They need to stay on the ground/
+         * maybe have different speeds depending on the role
+         */
+    }
+    void Throw()
+    {
+        //gets current position of the rock and throws a pebble in that direction
+        //pebbles reduce momentum by a small amount
+    }
+    void Jump()
+    {
+        //needs to move up and fall back down, should have gravity applied
+    }
+    void Crouch()
+    {
+        //changes to a crouching sprite, which gives it a smaller hitbox
+    }
+    void Panic()
+    {
+        //changes sprite to a panicking sprite, and just stands still
+    }
+    void Fly()
+    {
+        //just moves to the left in a straight line
+    }
+    void Charge()
+    {
+        //bird charges towards the rock
+    }
+}
--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine : MonoBehaviour{
+public class StateMachine {
     /* States:
 	 * Idle is just standing and doing nothing; used for initializing
 	 * Throw takes place when a Thrower is brave; a rock is thrown
@@ -74,89 +74,35 @@ public class StateMachine : MonoBehaviour{
             this.isBrave = Fuzzy.Linear(randomNum, lowerThreshold, upperThreshold);
             this.isAfraid = Fuzzy.NOT(isBrave);
         }
-        Enemy GenerateEnemy() //also Sets Role
+        public Enemy GenerateEnemy() //also Sets Role
         {
             Enemy enemy = new Enemy();
             float rand = Random.Range(0.0f, 10.0f);
-            if (rand < 5.0f && rand >= 2.5f) enemy.role = Role.THROWER;
-            else if (rand < 7.5f && rand >= 5.0f) enemy.role = Role.BRAWLER;
-            else if (rand < 10.0f && rand >= 7.5f) enemy.role = Role.NINJA;
-            else enemy.role = Role.TROLL;
+            if (rand < 5.0f && rand >= 2.5f) enemy.role = Enemy.Role.THROWER;
+            else if (rand < 7.5f && rand >= 5.0f) enemy.role = Enemy.Role.BRAWLER;
+            else if (rand < 10.0f && rand >= 7.5f) enemy.role = Enemy.Role.NINJA;
+            else enemy.role = Enemy.Role.TROLL;
             enemy.SetBravery(enemy.role);
             return enemy;
         }
-        Sprite GenerateSprite(Role role)
+        public Sprite GenerateSprite(Enemy.Role role)
         {
             Sprite sprite = new Sprite();
-            if (role == Role.UNASSIGNED) sprite = Resources.Load<Sprite>("Sprites/Goblins_0");
-            else if (role == Role.THROWER) sprite = Resources.Load<Sprite>("Sprites/GoblinChucker_0");
-            else if (role == Role.TROLL) sprite = Resources.Load<Sprite>("Sprites/GoblinTroll_0");
-            else if (role == Role.NINJA) sprite = Resources.Load<Sprite>("Sprites/GoblinNinja_0");
-            else if (role == Role.BRAWLER) sprite = Resources.Load<Sprite>("Sprites/GoblinBruiser_0");
-            else if (role == Role.BIRD) sprite = Resources.Load<Sprite>("Sprites/Bird_0");
+            if (role == Enemy.Role.UNASSIGNED) sprite = Resources.Load<Sprite>("Sprites/Goblins_0");
+            else if (role == Enemy.Role.THROWER) sprite = Resources.Load<Sprite>("Sprites/GoblinChucker_0");
+            else if (role == Enemy.Role.TROLL) sprite = Resources.Load<Sprite>("Sprites/GoblinTroll_0");
+            else if (role == Enemy.Role.NINJA) sprite = Resources.Load<Sprite>("Sprites/GoblinNinja_0");
+            else if (role == Enemy.Role.BRAWLER) sprite = Resources.Load<Sprite>("Sprites/GoblinBruiser_0");
+            else if (role == Enemy.Role.BIRD) sprite = Resources.Load<Sprite>("Sprites/Bird_0");
             return sprite;
         }
-        void SetState()
-        {
-            //first get role
-        }
-	    void Idle()
-        {
-            /* just stands still and detects if a rock is nearby
-             * if the rock is very far away, as in this enemy is now far off screen...
-             * we should call a destructor to get rid of this ai so we're not taking up excess resources
-             * Destroy(this);
-             */
-        }
-        void Attack()
-        {
-            //gets the position of the rock and moves towards it. 
-            //when at a close distance, throw a punch.
-        }
-        void Punch()
-        {
-            //uses a punching animation, and reduces momentum more significantly than a throw if it hits.
-
-        }
-        void Run()
-        {
-            /* gets position of the rock and moves away from it, might need to be careful to not
-             * let them move into the air. They need to stay on the ground/
-             * maybe have different speeds depending on the role
-             */
-        }
-        void Throw()
-        {
-            //gets current position of the rock and throws a pebble in that direction
-            //pebbles reduce momentum by a small amount
-        }
-        void Jump()
-        {
-            //needs to move up and fall back down, should have gravity applied
-        }
-        void Crouch()
-        {
-            //changes to a crouching sprite, which gives it a smaller hitbox
-        }
-        void Panic()
-        {
-            //changes sprite to a panicking sprite, and just stands still
-        }
-        void Fly()
-        {
-            //just moves to the left in a straight line
-        }
-        void Charge()
-        {
-            //bird charges towards the rock
-        }
-        float GetDistance(object sender, GameObject Rock)
+        public float GetDistance(object sender, GameObject Rock)
         {
             float x1 = ((GameObject)sender).transform.position.x;
             float y1 = ((GameObject)sender).transform.position.y;
             float x2 = Rock.transform.position.x;
             float y2 = Rock.transform.position.y;
-            return Mathf.Sqrt(((x1 - x2)*(x1 - x2)) + ((y1 - y2)*(y1 - y2)));
+            return Mathf.Sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
         }
     }
 }
