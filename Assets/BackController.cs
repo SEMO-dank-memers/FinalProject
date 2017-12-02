36bf2451255043cfff9cb1c79bd4f6cf704688fd
Bryant <jtbryant1s@semo.edu>
2017-12-02 14:00:23 -0600

    Added menu screen, how to play, and contributor screen

---
+++ Assets/BackController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackController : MonoBehaviour {
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Menu Screen");
+    }
+}
