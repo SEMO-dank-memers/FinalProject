2f21e5f42f766213a0f5c59325a210e1e56643b1
rocke97 <aarongunther97@gmail.com>
2017-12-03 20:54:53 -0600

    Added distance GUI
    The distance gui works as a max distance counter and won't count the
    ball rolling backwards in the game. This is to give some small goal to
    our prototype

---
+++ Assets/Scripts/DistanceUpdater.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceUpdater : MonoBehaviour {
+	int distance=10, previousDistance=0;
+	public GameObject player;
+	void Update () {
+		if (previousDistance < distance) {
+			this.GetComponent<Text> ().text = " Distance: " + (distance - 10);
+			previousDistance = distance;
+		}
+		distance = (int)player.transform.position.x;
+	}
+}
