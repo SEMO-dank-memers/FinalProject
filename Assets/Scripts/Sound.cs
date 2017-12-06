0ad6316220b7f56f59a42933f3cc335e4b6547ea
Ranciglio <mrranciglio1s@semo.edu>
2017-12-05 21:30:36 -0600

    Change sound script location

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+    public AudioClip BGmusic;
+    private AudioSource source;
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+		source.PlayOneShot(BGmusic);
+    }
+}
