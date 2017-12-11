cab7a8fdbe0372e6ff4ff82f76f5178be337108a
rocke97 <aarongunther97@gmail.com>
2017-12-10 20:43:08 -0600

    Fixed into screen
    The intro screen will now actually play the video

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+	public VideoPlayer vid;
+
+	void Update () {
+		if (!vid.isPlaying) {
+			SceneManager.LoadScene ("Menu Screen");
+		}
+	}
+}
